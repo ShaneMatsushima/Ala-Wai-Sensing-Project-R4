c3012855a41d873058216f9fe1c955ed477c6141
Shane Matsushima <skm1901@iolani.org>
2018-10-23 18:09:03 -1000

    Update NodeMCU_Send_Code.ino

--- Source/LoRa Project/Sending/NodeMCU_Send_Code.ino
+++ Source/LoRa Project/Sending/NodeMCU_Send_Code.ino
@@ -1,63 +1,42 @@
-#include <ESP8266WiFi.h>
+
 
 #include <HTTPSRedirect.h>
 
 #include <UbidotsMicroESP8266.h>
 
-#define ID1 "Temperature Sensor ID" 
-#define TOKEN "Put Ubidots TOKEN here"
+#define ID1 "5bbd1581c03f976992ecf059" 
+#define TOKEN "A1E-aQn8guUxXXoVL62ckFOw8y8yZkvtrk"
 
 Ubidots client1(TOKEN);
 
 //WiFi credentials
-#define WIFISSID "WiFi Name"
-#define PASSWORD "WiFi Password (Leave blank if you are on an unsecure connection)"
+#define WIFISSID "Palila"
+#define PASSWORD "io71redbird51"
 
-#define NAME "Hello World!"
+#define NAME "Iolani MonKit 1"
 
-// The ID below comes from Google Sheets.
-const char *GScriptId = "AKfycbycAHYDLwX3tB6NYYjtANRg1Lhs-sEg7xZb941NFLWVIwsxWzwt";
 
-// Push data on this interval
-const int dataPostDelay = 300000; 
-const char* host = "script.google.com";
-const char* googleRedirHost = "script.googleusercontent.com";
-
-const int httpsPort =  443;
-HTTPSRedirect client(httpsPort);
-String url = String("/macros/s/") + GScriptId + "/exec?";
-
-const char* fingerprint = "F0 5C 74 77 3F 6B 25 D7 3B 66 4D 43 2F 7E BC 5B E9 28 86 AD";
- 
 String sendvalue;
-float tempval;
+char tempval[6];
 
 void setup() {
   // put your setup code here, to run once:
 Serial.begin(9600);
+while(!Serial){;}
 client1.wifiConnection(WIFISSID, PASSWORD);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if(Serial.available()){
-  sendvalue = Serial.readString();
-  tempval = sendvalue.toFloat();
-  Serial.println(tempval);
+  //Serial.println("*");
+  if (Serial.available()){
+     Serial.readBytes(tempval, 6);
+Serial.println(tempval);
+     
+    //Serial.write(tempval);
+  
   client1.add(ID1, tempval);
   client1.sendAll();
-  postData(tempval);
-  }
-}
-
-void postData( float temps){
-  if (!client.connected()){
-    Serial.println("Connecting to client again..."); 
-    client.connect(host, httpsPort);
-  }
-  
-  String urlFinal = url + "&temps=" + String(temps) + "&name=" + String(NAME);
-  client.printRedir(urlFinal, host, googleRedirHost);
-  
+  } 
 }
